8399ee05cba6c8899d8f02d54c92c7f984d37e7e
Gabriel de Azevedo <gmarquesazti@gmail.com>
2022-07-30 16:23:09 -0300

    feat: add DashboardContainer

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -5,17 +5,36 @@ import { Flex, Center, Icon, Link } from '@chakra-ui/react';
 export const Header = () => {
   return (
     <Center>
-      <Flex m={6} width="90%" maxW="1024px" justify="space-between">
-        <Icon as={FaCompass} w={12} h={12} />
+      <Flex p={4} width="90%" maxW="1024px" justify="space-between">
+        <Icon color="blackAlpha.900" as={FaCompass} w={12} h={12} />
         <Center>
-          <Link as={RouterLink} to="/" fontSize={21} pl={4}>
+          <Link
+            color="blackAlpha.800"
+            as={RouterLink}
+            to="/"
+            fontSize={18}
+            pl={4}
+          >
             Dashboard
           </Link>
-          <Link fontSize={21} pl={4}>
-            Home
+          <Link
+            href="https://github.com/gabriel-de-azevedo/capital-empreendedor-test-frontend"
+            isExternal
+            color="blackAlpha.800"
+            fontSize={18}
+            pl={4}
+          >
+            Repo
           </Link>
-          <Link fontSize={21} pl={4} pr={4}>
-            About
+          <Link
+            href="https://github.com/gabriel-de-azevedo/capital-empreendedor-test-backend"
+            isExternal
+            color="blackAlpha.800"
+            fontSize={18}
+            pl={4}
+            pr={4}
+          >
+            API
           </Link>
         </Center>
       </Flex>
